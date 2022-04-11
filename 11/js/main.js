8f12972a7414111af11c02b868ea7b8551600040
Keks <keks@htmlacademy.ru>
2022-04-11 21:23:09 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,21 @@
+import {renderUsersPictures, showLoadFail} from './show-miniatures.js';
+import {closeEditFile} from './upload-file.js';
+import {loseSubmit, successSubmit} from'./submit-form.js';
+import {setUserFormSubmit} from './validate-form.js';
+import './scale-photo.js';
+import './filter-photo.js';
+import {getData} from './api.js';
+import './fullscreen-mode.js';
+import {showSortPhotosBlock, reShowPhotos} from './sort-photos.js';
+import {debounce} from './util.js';
+import './photo.js';
+
+const RERENDER_DELAY = 500;
+
+getData((photos) => {
+  renderUsersPictures(photos);
+  reShowPhotos(debounce(() => renderUsersPictures(photos),RERENDER_DELAY,));
+  showSortPhotosBlock();
+}, showLoadFail);
+
+setUserFormSubmit(successSubmit, loseSubmit, closeEditFile);
---
+++ 11/js/upload-file.js
@@ -0,0 +1,66 @@
+import {resetScale} from './scale-photo.js';
+import {resetEffect} from './filter-photo.js';
+
+const uploadInput = document.querySelector('#upload-file');
+const uploadOverlay = document.querySelector('.img-upload__overlay');
+const uploadCancel = document.querySelector('#upload-cancel');
+const textHashtags = document.querySelector('.text__hashtags');
+const textDescription = document.querySelector('.text__description');
+const scaleValue = document.querySelector('.scale__control--value');
+const userPhoto = document.querySelector('.img-upload__preview').querySelector('img');
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const onFormEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeEditFile();
+  }
+};
+
+const resetFilters = () => {
+  uploadInput.value = '';
+  textHashtags.value = '';
+  textDescription.value = '';
+  scaleValue.value = '100%';
+  resetScale();
+  userPhoto.style = 'transform: scale(1)';
+  resetEffect();
+};
+
+const openEditFile = () => {
+  uploadOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+};
+
+function closeEditFile () {
+  uploadOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  uploadCancel.removeEventListener('click', closeEditFile);
+  document.removeEventListener('keydown', onFormEscKeydown);
+  resetFilters();
+}
+
+textDescription.addEventListener('focus', () => {
+  document.removeEventListener('keydown', onFormEscKeydown);
+});
+
+textDescription.addEventListener('blur', () => {
+  document.addEventListener('keydown', onFormEscKeydown);
+});
+
+textHashtags.addEventListener('focus', () => {
+  document.removeEventListener('keydown', onFormEscKeydown);
+});
+
+textHashtags.addEventListener('blur', () => {
+  document.addEventListener('keydown', onFormEscKeydown);
+});
+
+uploadInput.addEventListener('change', () => {
+  openEditFile();
+  uploadCancel.addEventListener('click', closeEditFile);
+  document.addEventListener('keydown', onFormEscKeydown);
+});
+
+export {closeEditFile};
